8b7f8ab1858ac33ed39e7daaecdc2a478e0bc5d1
simonpf <simonpf@chalmers.se>
2017-12-16 09:44:46 +0100

    Update bash script to fetch data.

--- get_data.sh
+++ get_data.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-wget --recursive --no-parent http://spfrnd.de/data/inverse_theory_exercise_b/
+WGT="wget -nv"
+SRVR=http://spfrnd.de/data/inverse_theory_exercise_b/
+
+mkdir data
+mkdir data/python
+cd data/python
+${WGT} ${SRVR}data/python/gmi_tbs_0.npy
+${WGT} ${SRVR}data/python/gmi_tbs_1.npy
+${WGT} ${SRVR}data/python/iwp_database.npy
+${WGT} ${SRVR}data/python/iwp_validation.npy
+${WGT} ${SRVR}data/python/y_database.npy
+${WGT} ${SRVR}data/python/y_validation.npy
+
+cd ..
+mkdir matlab
+cd matlab
+
+${WGT} ${SRVR}data/matlab/exercise_b.mat
+
+cd ..
+
+${WGT} ${SRVR}data/plots/gmi_lats_0.npy
+${WGT} ${SRVR}data/plots/gmi_lats_1.npy
+${WGT} ${SRVR}data/plots/gmi_lons_0.npy
+${WGT} ${SRVR}data/plots/gmi_lons_1.npy
+${WGT} ${SRVR}data/plots/modis_img_0_extent.npy
+${WGT} ${SRVR}data/plots/modis_img_1_extent.npy
+${WGT} ${SRVR}data/plots/region_latlon.npy
+${WGT} ${SRVR}data/plots/region_xy.npy
+
+cd ../..
